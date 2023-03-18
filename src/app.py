f2f2a72a39b60ddba44a88c8180d557befd541c9
zoniha <awaawapurin@gmail.com>
2023-03-18 13:35:59 +0900

    feat: Add csv upload function

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import csv
+import time
 from typing import List
 
 import pyperclip
@@ -9,6 +11,9 @@ from utils.check_inputs import check_inputs
 from utils.generate_text import generate_text
 
 # Variables
+success_message = None
+if 'disable_submit' not in st.session_state:
+    st.session_state.disable_submit = True
 if 'generated_texts' not in st.session_state:
     st.session_state.generated_texts: List[Text] = []
 
@@ -23,39 +28,63 @@ for _ in range(5):
 # Generator section
 st.subheader('テキスト生成')
 
-col1, col2, col3 = st.columns([2, 3, 1], gap='medium')
+manual_tab, auto_tab = st.tabs(['自分で生成', 'ファイルから自動生成'])
 
-with col1:
-    input_label = st.text_input('ラベル', placeholder='半角英数字')
-with col2:
-    input_text = st.text_input('繰り返したい文字', value='', placeholder='埼玉県春日部市')
-with col3:
-    input_digits = st.text_input('桁数', value='', placeholder='10')
+with manual_tab:
+    with st.form('input-text'):
+        label_col, text_col, digits_col = st.columns([2, 3, 1], gap='medium')
+        with label_col:
+            input_label = st.text_input('ラベル', placeholder='半角英数字')
+        with text_col:
+            input_text = st.text_input('繰り返したい文字', value='', placeholder='埼玉県春日部市')
+        with digits_col:
+            input_digits = st.text_input('桁数', value='', placeholder='10')
 
+        generate_button = st.form_submit_button('テキストを生成')
 
-if st.button('生成'):
-    with st.spinner('生成中...'):
-        try:
-            check_inputs(input_text, input_digits)
-            digits = int(input_digits)
+        if generate_button:
+            with st.spinner('生成中...'):
+                try:
+                    check_inputs(input_text, input_digits)
+                    digits = int(input_digits)
 
-            if input_label:
-                label = input_label + ': ' + input_digits
-            else:
-                label = input_digits
-            label += '桁'
+                    if input_label:
+                        label = input_label + ': ' + input_digits
+                    else:
+                        label = input_digits
+                    label += '桁'
 
-            st.session_state.generated_texts.append(
-                Text(
-                    label=label,
-                    generated_text=generate_text(input_text, digits),
-                )
-            )
-        except ValueError:
-            st.error('桁数には数字を入力してください。')
-        except Exception as e:
-            st.error(e)
+                    st.session_state.generated_texts.append(
+                        Text(
+                            label=label,
+                            generated_text=generate_text(input_text, digits),
+                        )
+                    )
+                    success_message = st.success('生成完了🎉')
+                except ValueError:
+                    st.error('桁数には数字を入力してください。')
+                except Exception as e:
+                    st.error(e)
+with auto_tab:
+    with st.form('upload-csv'):
+        uploaded_file = st.file_uploader('CSVファイルをアップロード', type='csv')
+        upload_button = st.form_submit_button("テキストを生成")
+        if upload_button:
+            try:
+                with st.spinner('生成中...'):
+                    file_contents = uploaded_file.getvalue().decode("utf-8").splitlines()
+                    reader = csv.reader(file_contents)
 
+                    for row in reader:
+                        st.session_state.generated_texts.append(
+                            Text(
+                                label=row[0],
+                                generated_text=row[1],
+                            )
+                        )
+                    success_message = st.success('生成完了🎉')
+            except AttributeError:
+                st.error('CSVファイルをアップロードしてください🙇‍♂️')
 
 # Margin
 for _ in range(5):
@@ -65,6 +94,8 @@ for _ in range(5):
 st.subheader('生成済みテキスト')
 
 if (list_length := len(st.session_state.generated_texts)) > 0:
+    export_button = st.button('CSVファイルをエクスポート')
+
     for i in range(list_length):
         blank_line()
         st.text_area(
@@ -88,5 +119,8 @@ if (list_length := len(st.session_state.generated_texts)) > 0:
             if delete_button:
                 del st.session_state.generated_texts[delete_index]
                 st.experimental_rerun()
+    if success_message:
+        time.sleep(3)
+        success_message.empty()
 else:
     st.caption('生成済みのテキストはありません。')
